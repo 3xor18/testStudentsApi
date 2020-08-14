78e94342153e2e4c1ede2cfb40b9ba39165a3534
Gerson Aponte <3xor18@gmail.com>
2020-08-13 20:05:55 -0400

    initial project

---
+++ src/main/java/com/gersonAponte/app/GersonTestApplication.java
@@ -0,0 +1,13 @@
+package com.gersonAponte.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GersonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GersonTestApplication.class, args);
+	}
+
+}
